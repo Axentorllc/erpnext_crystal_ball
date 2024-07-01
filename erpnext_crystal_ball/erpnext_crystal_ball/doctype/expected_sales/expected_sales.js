834c2009894d7bfdea60806ee55ee77f8d88edad
Carlos9K <carlosashraf557@gmail.com>
2024-07-01 18:31:54 +0000

    add filter for item code

--- erpnext_crystal_ball/erpnext_crystal_ball/doctype/expected_sales/expected_sales.js
+++ erpnext_crystal_ball/erpnext_crystal_ball/doctype/expected_sales/expected_sales.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2024, Carlos and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Expected Sales", {
-// 	refresh(frm) {
 
-// 	},
-// });
+frappe.ui.form.on("Expected Sales", {
+    refresh(frm) {
+        console.log("--------------")
+        frm.set_query("item_code", "item_records",  function(){
+            return {
+                "filters": [["Item","item_group", "=", "FG"]]
+               
+            }
+        });
+	},
+});
+
+
