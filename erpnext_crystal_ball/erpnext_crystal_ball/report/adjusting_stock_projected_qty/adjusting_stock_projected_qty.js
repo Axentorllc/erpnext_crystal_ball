9b8e8f04073013c1d9f9d8f99d4cb96ea27b455b
Carlos9K <carlosashraf557@gmail.com>
2024-09-29 09:50:05 +0000

    Add: 'Safety Stock' and 'Fiscal Year' filters and Apply red color styling to 'Difference Quantity' when < 0

--- erpnext_crystal_ball/erpnext_crystal_ball/report/adjusting_stock_projected_qty/adjusting_stock_projected_qty.js
+++ erpnext_crystal_ball/erpnext_crystal_ball/report/adjusting_stock_projected_qty/adjusting_stock_projected_qty.js
@@ -22,8 +22,43 @@ frappe.query_reports["Adjusting Stock Projected Qty"] = {
 			label: __("Group_by"),
 			fieldtype: "Select",
 			options: "\nraw material\npack material",
-			// reqd:1
-			
+
 		},
-	]
+		{
+			fieldname: "fiscal_year",
+			label: __("Fiscal Year"),
+			fieldtype: "Link",
+			options: "Fiscal Year",
+			reqd:1
+		},
+		{
+			fieldname: "safety_stock",
+			label: __("Include Safty Stock"),
+			fieldtype: "Check",
+		},
+
+	],
+	formatter: function (value, row, column, data, default_formatter) {
+		
+		let months=["January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December"] 
+		
+			for (let i = 0; i < months.length; i++) {
+				let month = months[i];
+		
+				if (column.fieldname === `${month}_diff_qty`,value) {
+					let formatted_value = default_formatter(value, row, column, data);
+					
+					if (value < 0) {
+						formatted_value = "<span style='color:Red;'>" + formatted_value + "</span>";
+					}
+					
+					return formatted_value;
+				} 
+				
+			}
+			
+			return default_formatter(value, row, column, data);
+
+	},
 };
