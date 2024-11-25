c6c51f2dcb9b79115a277402e5955627b83ce393
Carlos9K <carlosashraf557@gmail.com>
2024-11-25 12:00:13 +0000

    Update naming logic in ProjectedBudget

--- erpnext_crystal_ball/erpnext_crystal_ball/doctype/projected_budget/projected_budget.py
+++ erpnext_crystal_ball/erpnext_crystal_ball/doctype/projected_budget/projected_budget.py
@@ -3,13 +3,20 @@
 
 import frappe
 from frappe import _
-from frappe.model.document import Document
-
+from frappe.model.document import Document       
+from datetime import datetime
 class ProjectedBudget(Document):
     
     def validate(self):
         # Replace 'start_date' and 'end_date' with the field names you want to validate
         self.validate_from_to_dates('start_date', 'end_date')
-       
+        self.before_save_naming()
+    
 
+    def before_save_naming(self):
+
+        if self.start_date and self.fiscal_year:
+            month_name = datetime.strptime(self.start_date, '%Y-%m-%d').strftime('%B')
+            
+            self.name = f"PB-{self.fiscal_year}-{month_name[:3]}"
    
