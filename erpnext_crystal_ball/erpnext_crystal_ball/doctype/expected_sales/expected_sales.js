786a55be2ab95059f4e5a379e458008ca3b6c4ee
Carlos9K <carlosashraf557@gmail.com>
2025-04-15 15:59:29 +0000

    fix: auto-fetch UOM based on Item code

--- erpnext_crystal_ball/erpnext_crystal_ball/doctype/expected_sales/expected_sales.js
+++ erpnext_crystal_ball/erpnext_crystal_ball/doctype/expected_sales/expected_sales.js
@@ -1,17 +1,35 @@
-// Copyright (c) 2024, Carlos and contributors
+// Copyright (c) 2024, Axentor and contributors
 // For license information, please see license.txt
 
 
 frappe.ui.form.on("Expected Sales", {
     refresh(frm) {
-        console.log("--------------")
+
         frm.set_query("item_code", "item_records",  function(){
             return {
                 "filters": [["Item","item_group", "=", "FG"]]
                
             }
         });
+
+        console.log(frm)
 	},
+    
+})
+frappe.ui.form.on('Item records', {
+    
+    before_save: function(frm) {
+        frm.doc.item_records.forEach(function(row) {
+            // If item_code is available in the row, fetch the stock_uom
+            if (row.item_code) {
+                frappe.db.get_value('Item', row.item_code, 'stock_uom')
+                    .then(r => {
+                        if (r.message && r.message.stock_uom) {
+                            // Set the stock_uom in the row
+                            frappe.model.set_value(row.doctype, row.name, 'stock_uom', r.message.stock_uom);
+                        }
+                    });
+            }
+        });
+    }
 });
-
-
