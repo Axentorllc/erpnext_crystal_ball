e107a25bb0e08ccaed8b77554b6ae7890df60fc1
Carlos9K <carlosashraf557@gmail.com>
2025-05-11 11:08:07 +0000

    feat: filter team members by selected shift in 'Production Team' dt

--- erpnext_crystal_ball/capacity_planning/doctype/production_team/production_team.js
+++ erpnext_crystal_ball/capacity_planning/doctype/production_team/production_team.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2025, Carlos and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Production Team", {
-// 	refresh(frm) {
+frappe.ui.form.on("Production Team", {
+	refresh(frm) {
 
-// 	},
-// });
+        frm.set_query("member", "team_member",  function(){
+            return {
+                "filters": [["Employee","default_shift", "=", frm.doc.shift]]
+               
+            }
+        });
+
+	},
+});
